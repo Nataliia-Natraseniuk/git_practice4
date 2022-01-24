20836387e425d37b3ac91f755b2193260cc0911a
natal <nataliia1228@gmail.com>
2022-01-24 10:38:31 -0500

    initial commit 3

--- src/Hello/Hello1.java
+++ src/Hello/Hello1.java
@@ -3,6 +3,8 @@ package Hello;
 public class Hello1 {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        // Hello B25
+        // Learning is fun
     }
 
 }
